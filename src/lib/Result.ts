6ff6bb0a1c425ac9885e01d7a09e4384cc8b13dd
nehu3n <developer.nh3@gmail.com>
2024-08-19 21:45:46 -0300

    ✨ feat(lib): Added functions to Result.
    Functions such as unwrap, unwrapElse, expect and andThen were added to the Result type.

--- src/lib/Result.ts
+++ src/lib/Result.ts
@@ -32,5 +32,46 @@ function err<E>(error: E): Result<never, E> {
   return new Err(error);
 }
 
+function unwrap<T, E>(result: Result<T, E>): T {
+  if (result instanceof Ok) {
+    return result.value;
+  }
+  throw result.error;
+}
+
+function unwrapOr<T, E>(result: Result<T, E>, defaultValue: T): T {
+  if (result instanceof Ok) {
+    return result.value;
+  }
+  return defaultValue;
+}
+
+function unwrapOrElse<T, E>(
+  result: Result<T, E>,
+  fallback: (error: E) => T
+): T {
+  if (result instanceof Ok) {
+    return result.value;
+  }
+  return fallback(result.error);
+}
+
+function expect<T, E>(result: Result<T, E>, message: string): T {
+  if (result instanceof Ok) {
+    return result.value;
+  }
+  throw new Error(message);
+}
+
+function andThen<T, E, U>(
+  result: Result<T, E>,
+  fallback: (value: T) => Result<U, E>
+): Result<U, E> {
+  if (result instanceof Ok) {
+    return fallback(result.value);
+  }
+  return result as Err<E>;
+}
+
 export default Result;
-export { ok, err };
+export { ok, err, unwrap, unwrapOr, unwrapOrElse, expect, andThen };
