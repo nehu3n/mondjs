7e23e5f845d3452507d42b1c9f86cd894f5c0065
nehu3n <developer.nh3@gmail.com>
2024-08-20 09:24:01 -0300

    ✨ feat(option): Added the Option type with its methods.
    The Option type was added with its methods. It represents a possible existing value. It is more powerful than using null or undefined, since it forces you to handle it explicitly.

---
+++ src/lib/Option.ts
@@ -0,0 +1,58 @@
+class Some<T> {
+  constructor(public value: T) {}
+
+  public isSome(): boolean {
+    return true;
+  }
+
+  public isNone(): boolean {
+    return false;
+  }
+
+  public unwrap(): T {
+    return this.value;
+  }
+
+  public unwrapOr(defaultValue: T): T {
+    return this.value;
+  }
+
+  public expect(message: string): T {
+    return this.value;
+  }
+}
+
+class None {
+  public isSome(): boolean {
+    return false;
+  }
+
+  public isNone(): boolean {
+    return true;
+  }
+
+  public unwrap(): never {
+    throw new Error("Called unwrap on a None value.");
+  }
+
+  public unwrapOr<T>(defaultValue: T): T {
+    return defaultValue;
+  }
+
+  public expect(message: string): never {
+    throw new Error(message);
+  }
+}
+
+type Option<T> = Some<T> | None;
+
+function some<T>(value: T): Option<T> {
+  return new Some(value);
+}
+
+function none(): Option<never> {
+  return new None();
+}
+
+export default Option;
+export { some, none };
